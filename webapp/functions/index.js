752bafc752352a2b1342cf1581d4f907e4375eab
anamiz <ana.maia97@hotmail.com>
2019-12-11 01:10:24 +0000

    percentage and current people working

--- webapp/functions/index.js
+++ webapp/functions/index.js
@@ -22,16 +22,34 @@ var rootRef = firebase.database().ref();
 
 var database = firebase.database();
 
- exports.helloWorld = functions.https.onRequest((request, response) => {
+ exports.ammountPeople = functions.https.onRequest((request, response) => {
    const dbRefObject =firebase.database().ref('sensor/dht/');
-   dbRefObject.limitToLast(1).on('value',snap=> response.send(snap.val()));
-   });
+   dbRefObject.limitToLast(1).on('value',snap=> {snap.forEach(childSnapshot => {let quantity = childSnapshot.child('counterIn');response.send(quantity);});});});
 
-   exports.helloWorld2 = functions.https.onRequest((request, response) => {
-     const dbRefObject =firebase.database().ref('sensor/dht');
-     dbRefObject.on('value', function(snapshot){
-       snapshot.forEach(function(childSnapshot){
-        response.send(childSnapshot.val());
-      });
+
+
+   exports.percentagePeople = functions.https.onRequest((request, response) => {
+    const dbRefObject =firebase.database().ref('sensor/dht/');
+    var maxPeopleInRoom = 200; //valor arbitrario por agora
+    dbRefObject.limitToLast(1).on('value',snap=> {snap.forEach(childSnapshot => {let quantity = childSnapshot.child('counterIn');
+    var quantityJs = quantity.val();
+    var percentage = (quantityJs / maxPeopleInRoom) * 100;
+    response.send('<p>' + percentage + '%' + '</p>');});});});   //nao podemos mandar um numero apenas pelo send, daí ter metido num paragrafo
+
+ 
+ 
+  exports.maxPeople = functions.https.onRequest((request, response) => {
+    const dbRefObject =firebase.database().ref('sensor/dht');
+    let max = 0;
+
+    dbRefObject.on('value', function(snapshot){
+      let dataValues = snapshot.exportVal();
+      for(var prop in dataValues)
+      {
+        if(Object.prototype.hasOwnProperty.call(dataValues, prop))
+        {
+          response.send(dataValues[prop]);
+        }
+      }
     });
    });
