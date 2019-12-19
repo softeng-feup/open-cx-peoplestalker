565e59377798face3b7eb1abd09b2ac3c1dc0601
RMMPP <ei01020@fe.up.pt>
2019-12-19 17:35:37 +0000

    hotfix adminForm

--- webapp/public/scripts/auth.js
+++ webapp/public/scripts/auth.js
@@ -3,7 +3,7 @@
 const adminForm = document.querySelector('.admin-actions');
 adminForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    const adminEmail = document.querySelector('#admin-email').value;
+    var adminEmail = document.querySelector('#admin-email').value;
     const addAdminRole = functions.httpsCallable('addAdminRole');
     addAdminRole({email: adminEmail}).then(result => {
         console.log(result);
